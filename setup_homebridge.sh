515fa90009edfd1cd5280c51898dae379940c956
Ajaykumar Kannan <kannan.ajay@gmail.com>
2020-10-02 21:39:47 -0400

    Made the scripts much simpler for now

--- setup_homebridge.sh
+++ setup_homebridge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker pull oznu/homebridge
-docker run --net=host --name=homebridge -e PUID=1000 -e PGID=1000  -e HOMEBRIDGE_CONFIG_UI=1   -e HOMEBRIDGE_CONFIG_UI_PORT=8080   -v ~/homebridge:/homebridge   oznu/homebridge
+docker run -d --restart=always --net=host --name=homebridge -e PUID=1000 -e PGID=1000  -e HOMEBRIDGE_CONFIG_UI=1   -e HOMEBRIDGE_CONFIG_UI_PORT=8080   -v ~/homebridge:/homebridge   oznu/homebridge
