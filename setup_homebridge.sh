a52e77676c16f690f1b525b92851fb859a459103
Ajaykumar Kannan <kannan.ajay@gmail.com>
2020-10-14 01:59:18 -0400

    Slightly improved the docker set up scripts

--- setup_homebridge.sh
+++ setup_homebridge.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 docker pull oznu/homebridge
+docker stop homebridge
+docker rm homebridge
 docker run -d --restart=always --net=host --name=homebridge -e PUID=1000 -e PGID=1000  -e HOMEBRIDGE_CONFIG_UI=1   -e HOMEBRIDGE_CONFIG_UI_PORT=8080   -v ~/homebridge:/homebridge   oznu/homebridge
+docker rmi $(docker images -f "dangling=true" -q)
--- setup_or_update_HomeAssistant.sh
+++ setup_or_update_HomeAssistant.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker pull homeassistant/home-assistant:stable
+docker stop home-assistant
+docker rm home-assistant
 docker run -d --restart=always --name="home-assistant" -v /home/pi/home-assistant:/config -v /etc/localtime:/etc/localtime:ro --net=host homeassistant/home-assistant:stable  # finally, start a new one
 docker rmi $(docker images -f "dangling=true" -q)
