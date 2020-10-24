1a46d8899a9387eed81432845ca97996860f6998
Ajaykumar Kannan <kannan.ajay@gmail.com>
2020-10-24 16:36:12 -0400

    Added undervolt check script

--- rpi_setup.sh
+++ rpi_setup.sh
@@ -14,52 +14,26 @@ rm ~/.bashrc
 for file in ~/src/bash_utils/.*
 do
   if [ -f $file ]; then
-    ln -s $i
+    ln -s $file
   fi
 done
 
-if [ ! -f `which pihole` ]; then
+if [ `which pihole` ]; then
+	echo "pihole already installed, skipping"
+else
 	echo "Setting up pihole"
 	curl -sSL https://install.pi-hole.net | sudo bash
-else
-	echo "pihole already installed, skipping"
 fi
 
-if [ ! -f `which docker` ]; then
+if [ `which docker` ]; then
+	echo "Docker already installed, skipping"
+else
 	echo "Setting up docker"
 	curl -fsSL https://get.docker.com | sudo bash
 	sudo usermod -aG docker pi
 	sudo usermod -aG docker root
-else
-	echo "Docker already installed, skipping"
 fi
 
-sudo echo "tmpfs /tmp     tmpfs defaults,noatime,nosuid,size=100m           0 0" >> /etc/fstab
-sudo echo "tmpfs /var/tmp tmpfs defaults,noatime,nosuid,size=30m            0 0" >> /etc/fstab
-sudo echo "tmpfs /var/log tmpfs defaults,noatime,nosuid,mode=0755,size=100m 0 0" >> /etc/fstab
-sudo echo "tmpfs /var/run tmpfs defaults,noatime,nosuid,mode=0755,size=2m   0 0" >> /etc/fstab
-
-sudo mkdir /media/SJ_EXT/
-SJ_UUID=`sudo blkid  | grep "S_AJ_EXT2"  | sed 's|.*PARTUUID="\(\S.*\)".*|\1|g'`
-sudo echo "PARTUUID=$SJ_UUID /media/SJ_EXT/  exfat defaults,auto,umask=000,users,rw 0 0" >> /etc/fstab
-sudo mount -a
-
-sudo vim /etc/samba/smb.conf
-sudo smbpasswd -a pi
-sudo echo "[pishare]" >> /etc/samba/smb.conf
-sudo echo "path = /media/SJ_EXT" >> /etc/samba/smb.conf
-sudo echo "writeable=Yes" >> /etc/samba/smb.conf
-sudo echo "create mask=0777" >> /etc/samba/smb.conf
-sudo echo "directory mask=0777" >> /etc/samba/smb.conf
-sudo echo "public=no" >> /etc/samba/smb.conf
-sudo systemctl restart smbd
-
-sudo hdparm -y /dev/sda
-sudo hdparm -B127 /dev/sda
-sudo echo "/dev/sda {" >> /etc/hdparm.conf
-sudo echo "write_cache = on" >> /etc/hdparm.conf
-sudo echo "spindown_time = 120" >> /etc/hdparm.conf
-sudo echo "}" >> /etc/hdparm.conf
 
 sudo apt-get remove --auto-remove --purge wolfram-engine libreoffice chromium-browser x11-common libx11-.* -y
 sudo apt-get autoremove  -y
