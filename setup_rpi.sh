4a43b256dae2a67a1b42926a711b0971844c7528
Ajaykumar Kannan <kannan.ajay@gmail.com>
2020-10-24 15:22:34 -0400

    More fixes for the first setup script

--- setup_rpi.sh
+++ setup_rpi.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
 echo "Getting basic utils"
-sudo apt install -y tmux screen vim build-essential exfat-utils htop ncdu samba samba-common-bin hdparm
+sudo apt install -y tmux screen vim build-essential exfat-utils htop ncdu samba samba-common-bin hdparm unbound nload
 
-echo "Setting up pihole"
-curl -sSL https://install.pi-hole.net | sudo bash
+git config --global user.email "kannan.ajay@gmail.com"
+git config --global user.name "Ajaykumar Kannan"
 
-echo "Setting up docker"
-curl -fsSL https://get.docker.com | sudo bash
-sudo usermod -aG docker pi
-sudo usermod -aG docker root
+git clone https://github.com/markgandolfo/git-bash-completion.git ~/src/git-bash-completion
+git clone git@github.com:ajaykumarkannan/bash_utils.git ~/src/bash_utils/
+
+cd ~/
+rm ~/.bashrc
+for file in ~/src/bash_utils/.*
+do
+  if [ -f $file ]; then
+    ln -s $i
+  fi
+done
+
+if [ ! -f `which pihole` ]; then
+	echo "Setting up pihole"
+	curl -sSL https://install.pi-hole.net | sudo bash
+else
+	echo "pihole already installed, skipping"
+fi
+
+if [ ! -f `which docker` ]; then
+	echo "Setting up docker"
+	curl -fsSL https://get.docker.com | sudo bash
+	sudo usermod -aG docker pi
+	sudo usermod -aG docker root
+else
+	echo "Docker already installed, skipping"
+fi
 
 sudo echo "tmpfs /tmp     tmpfs defaults,noatime,nosuid,size=100m           0 0" >> /etc/fstab
 sudo echo "tmpfs /var/tmp tmpfs defaults,noatime,nosuid,size=30m            0 0" >> /etc/fstab
@@ -18,7 +41,7 @@ sudo echo "tmpfs /var/run tmpfs defaults,noatime,nosuid,mode=0755,size=2m   0 0"
 
 sudo mkdir /media/SJ_EXT/
 SJ_UUID=`sudo blkid  | grep "S_AJ_EXT2"  | sed 's|.*PARTUUID="\(\S.*\)".*|\1|g'`
-sudo echo "PARTUUID=$(SJ_UUID) /media/SJ_EXT/  exfat defaults,auto,umask=000,users,rw 0 0" >> /etc/fstab
+sudo echo "PARTUUID=$SJ_UUID /media/SJ_EXT/  exfat defaults,auto,umask=000,users,rw 0 0" >> /etc/fstab
 sudo mount -a
 
 sudo vim /etc/samba/smb.conf
@@ -38,22 +61,7 @@ sudo echo "write_cache = on" >> /etc/hdparm.conf
 sudo echo "spindown_time = 120" >> /etc/hdparm.conf
 sudo echo "}" >> /etc/hdparm.conf
 
-sudo apt-get remove --purge wolfram-engine libreoffice chromium-browser x11-common -y
-sudo apt-get remove --purge wolfram-engine libreoffice -y
-sudo apt-get remove --auto-remove --purge libx11-.* -y
+sudo apt-get remove --auto-remove --purge wolfram-engine libreoffice chromium-browser x11-common libx11-.* -y
 sudo apt-get autoremove  -y
+sudo apt-get autoclean  -y
 
-git config --global user.email "kannan.ajay@gmail.com"
-git config --global user.name "Ajaykumar Kannan"
-
-git clone https://github.com/markgandolfo/git-bash-completion.git ~/src/git-bash-completion
-git clone git@github.com:ajaykumarkannan/bash_utils.git ~/src/bash_utils/
-
-cd ~/
-rm ~/.bashrc
-for file in ~/src/bash_utils/.*
-do
-  if [ -f $file ]; then
-    ln -s $i
-  fi
-done
